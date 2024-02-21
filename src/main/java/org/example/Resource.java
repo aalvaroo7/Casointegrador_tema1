ae732dd1c8c9f2d3c2f88005d38e36a622114cd8
aalvaroo7 <almarrom@myuax.com>
2024-02-21 11:16:46 +0100

    comit

--- src/main/java/org/example/Resource.java
+++ src/main/java/org/example/Resource.java
@@ -1,4 +1,19 @@
 package org.example;
 
 public class Resource {
+    private String name;
+    private double dailyConsumption; // daily consumption rate
+
+    public Resource(String name, double dailyConsumption) {
+        this.name = name;
+        this.dailyConsumption = dailyConsumption;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getDailyConsumption() {
+        return dailyConsumption;
+    }
 }
